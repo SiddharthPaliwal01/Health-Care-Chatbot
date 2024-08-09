e99ec58ea7ab5dfa2b68a3bad489e496fe356c64
Siddharth Paliwal <103729891+SiddharthPaliwal01@users.noreply.github.com>
2024-08-09 16:17:17 +0530

    Add

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Healthcare Chatbot',
+    version= '0.0.0',
+    author= 'Siddharth Paliwal',
+    author_email= 'siddharthpaliwal23@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
